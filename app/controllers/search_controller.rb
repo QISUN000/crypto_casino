cd678b9c42ccaf23cc7a3dba1514f17fca778772
QI SUN <lukesun2023@gmail.com>
2025-03-05 17:58:33 -0600

    search upgrated

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,26 @@
 class SearchController < ApplicationController
   def index
     @query = params[:q]
+    @category_id = params[:category]
+    @categories = Category.all.order(:name)
     
     if @query.present?
-      @cryptocurrencies = Cryptocurrency.where("name ILIKE ? OR symbol ILIKE ? OR description ILIKE ?", 
-                                            "%#{@query}%", "%#{@query}%", "%#{@query}%")
-                                     .order(rank: :asc)
+      # Start with a base query
+      @cryptocurrencies = Cryptocurrency.order(rank: :asc)
       
-  
+      # Filter by category if provided
+      if @category_id.present?
+        @category = Category.find_by(id: @category_id)
+        if @category
+          @cryptocurrencies = @cryptocurrencies.joins(:categories).where(categories: { id: @category_id })
+        end
+      end
+      
+      # Apply text search
+      @cryptocurrencies = @cryptocurrencies.where(
+        "name ILIKE ? OR symbol ILIKE ? OR description ILIKE ?", 
+        "%#{@query}%", "%#{@query}%", "%#{@query}%"
+      )
     else
       @cryptocurrencies = []
     end
